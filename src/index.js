afca63fabb4825521988b53dcc7d48c403183695
tpence06 <tpence06@hotmail.com>
2023-03-09 09:07:01 -0700

    final stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-let addToy = false;
-
-document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  let addToy = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const BASE_URL = "http://localhost:3000/toys"
+
+fetch(BASE_URL)
+  .then(res => res.json())
+  .then(toys => {
+    let toysHTML = toys.map(function() {
+    return `
+    <div class="card">
+      <h2>${toys.name}</h2>
+      <img src=${toys.image} class="toy-avatar" />
+      <p>${toys.likes} Likes </p>
+      <button class="like-btn" id="[toy_id]">Like ❤️</button>
+    </div>
+    `
+  })
+ document.querySelector('#toy-collection').innerHTML =toysHTML
+  })
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +32,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
